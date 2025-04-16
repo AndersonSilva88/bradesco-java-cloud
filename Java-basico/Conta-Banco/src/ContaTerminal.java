e2b59e509102d8286acb93927cf5d690e04f4eb8
AndersonOSilva <anderson.os88@hotmail.com>
2025-04-16 19:56:59 -0300

    projeto conta bancária utilizando a classe scanner

--- Java-basico/Conta-Banco/src/ContaTerminal.java
+++ Java-basico/Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+        Scanner scan = new Scanner(System.in);
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo = 100.00;
+        //Exibir as mensagens para o nosso usuário
+        System.out.println("***********************************");
+        System.out.println("***** SEJA BEM VINDO DIOBANK ******");
+        System.out.println("***********************************");
+        //Obter pela scanner os valores digitados no terminal
+        System.out.println("Digite seu nome: ");
+        nomeCliente = scan.nextLine();
+        System.out.println("Digite sua agencia: ");
+        agencia = scan.nextLine();
+        System.out.println("Digite o numero da conta bancária: ");
+        numero = scan.nextInt();
+        //Exibir a mensagem conta criada
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+
+                ", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
     }
 }
